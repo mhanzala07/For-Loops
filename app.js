f87a08c93694e7cdf28bc77a2b76313e17752cf2
mhanzala07 <hanzalawaseem073@gmail.com>
2024-03-27 00:41:09 +0500

    index file created

---
+++ app.js
@@ -0,0 +1,45 @@
+// Question # 1
+// Write a program to display the message "Hello World" 5 times in your browser using for loop.
+
+for (let i = 1; i <= 5; i++){
+    console.log(`Hello World` , i);
+}
+
+// Question # 2
+// Write a program to print numeric counting from 1 to 10.
+
+for (let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+// Question # 3
+// Write a program to print multiplication table of any number using for loop. Table number & length should be taken as an input form user.
+
+// let userInput = prompt ('Enter Table Number')
+// let time = prompt ('How many time do you want?')
+// for (let i = 1; i <= 10; i++){
+//     console.log(`${userInput} * ${i} = ${userInput*i}`);
+// }
+
+// Question # 4
+// You have an array A = ["Nokia" , "Samsung" , "Apple" , "Sony" , "Huawei"] Write each element on new line with the help of for loop.
+
+// let brand = ["Nokia" , "Samsung" , "Apple" , "Sony" , "Huawei"]
+// console.log(brand);
+// let ul = document.querySelector('ul');
+// for (let i = 0; i < brand.length; i++){
+//     console.log(brand[i]);
+//     ul.innerHTML += `<li>${brand[i]} </li>`
+// }
+
+// Question # 5
+// Write a program to print items of the following arraw using for loop:
+// fruits = ["apple" , "banana" , "mango" , "orange" , "strawberry"]
+
+let fruits = ["apple" , "banana" , "mango" , "orange" , "strawberry"]
+console.log(fruits);
+for (let i = 0; i < fruits.length; i++){
+    console.log(fruits[i]);
+    ul.innerHTML += `<li>${fruits[i]} </li>`
+    console.log([i])
+}
